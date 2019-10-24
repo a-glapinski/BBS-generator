8e0f9e61cf251be58dec1cc4f25179eb3ca6d366
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2019-10-24 21:39:38 +0200

    Implement generator

---
+++ src/main/kotlin/BBSGenerator.kt
@@ -0,0 +1,40 @@
+import kotlin.math.sqrt
+
+class BBSGenerator(
+    p: Long,
+    q: Long,
+    /** Seed */
+    private val x: Long
+) {
+    private val n = p * q
+
+    init {
+        require(p.isPrime() && q.isPrime()) { "P and Q have to be prime numbers." }
+        require(p % 4 == 3L && q % 4 == 3L) { "Both first and second prime has to be congruent to 3 mod 4." }
+        require(x.isCoprimeTo(n)) { "X must be coprime to N = P * Q." }
+    }
+
+    fun generateSequence(length: Int): String {
+        var generatedValue = x.squared() % n
+
+        return buildString {
+            for (i in 0 until length) {
+                append(generatedValue.lsb())
+                generatedValue = generatedValue.squared() % n
+            }
+        }
+    }
+
+    private fun Long.lsb() = this.toString(radix = 2).last()
+}
+
+private fun Long.isPrime(): Boolean = (2..sqrt(this.toDouble()).toLong()).none { this % it == 0L }
+
+private tailrec fun greatestCommonDivisor(a: Long, b: Long): Long {
+    if (b == 0L) return a
+    return greatestCommonDivisor(b, a % b)
+}
+
+private fun Long.isCoprimeTo(other: Long): Boolean = greatestCommonDivisor(this, other) == 1L
+
+private fun Long.squared() = this * this
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
-fun main() {
+const val P = 11L
+const val Q = 19L
+const val X = 13L
 
+fun main() {
+    val bbsGenerator = BBSGenerator(P, Q, X)
+    val sequence = bbsGenerator.generateSequence(length = 20000)
+
+    println(sequence)
 }
